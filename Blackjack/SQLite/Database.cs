36b9e97e42896fe492abda1164b1b298a7ceef26
Alejandro Vera <Alejandro.vera-gonzalez@trojans.dsu.edu>
2019-12-03 19:41:58 -0600

    SQLite Constructure, Connect(), and Disconnect() made

---
+++ Blackjack/SQLite/Database.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+
+namespace SQLite
+{
+    public class Database
+    {
+        private SQLiteConnection _fileConnection { get; set; }
+        private string constr { get; set; }
+        private readonly string _sqlitePath;
+
+        public Database(string FilePath)
+        {
+            _sqlitePath = FilePath;
+            constr = $"Data Source={_sqlitePath};Version=3;";
+
+            if (!File.Exists(_sqlitePath))
+            {
+                SQLiteConnection.CreateFile(_sqlitePath);
+                Debug.WriteLine("~SQLite file created.");
+            }
+
+            _fileConnection = new SQLiteConnection(constr);
+        }
+
+        /// <summary>
+        /// Creates connection to the SQlite db file.
+        /// </summary>
+        /// <returns>
+        /// Returns if connection was successfully made.
+        /// </returns>
+        public bool Connect()
+        {
+            if (File.Exists(_sqlitePath) && _fileConnection.State != ConnectionState.Open)
+            {
+                _fileConnection.Open();
+                Debug.WriteLine("~File connection made.");
+                return true;
+            }
+            else
+            {
+                if(!File.Exists(_sqlitePath))
+                {
+                    Debug.WriteLine("~File connection was not made. File not found.");
+                    return false;
+                }
+
+                if(_fileConnection.State == ConnectionState.Open)
+                {
+                    Debug.WriteLine("~File connection was not made. File connection is already open");
+                    return false;
+                }
+
+                Debug.WriteLine("~File connection was not made due to unknown error");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Safely closes connection to SQLite db file.
+        /// </summary>
+        /// <returns>
+        /// Returns if connection closure was successful.
+        /// </returns>
+        public bool Disconnect()
+        {
+            if(File.Exists(_sqlitePath) && _fileConnection.State != ConnectionState.Closed)
+            {
+                _fileConnection.Close();
+                Debug.WriteLine("~Sqlite db connection closed successfully.");
+                return true;
+            }
+            else
+            {
+                if (!File.Exists(_sqlitePath))
+                {
+                    Debug.WriteLine("~Did not close file. File not found.");
+                    return false;
+                }
+
+                if (_fileConnection.State == ConnectionState.Closed)
+                {
+                    Debug.WriteLine("~Could not close file. File connection is already closed");
+                    return false;
+                }
+
+                Debug.WriteLine("~File closure was not made due to unknown error");
+                return false;
+            }
+        }
+    }
+}

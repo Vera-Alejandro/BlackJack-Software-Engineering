c28d36141cabc7220035a084fe97ff3a5f9d902e
Alejandro <Alejandro.vera-gonzalez@trojans.dsu.edu>
2020-06-27 19:21:33 -0500

    Switched Card images from bitmapimage to the string filepath

--- Blackjack/Blackjack.Data/Card.cs
+++ Blackjack/Blackjack.Data/Card.cs
@@ -1,267 +1,51 @@
 ﻿using Blackjack.Data.Enums;
-using Blackjack.Data.Properties;
-using System.Windows.Media.Imaging;
+using System;
+using System.IO;
 using System.Windows.Media;
-using System.Drawing;
+using System.Windows.Media.Imaging;
 
 namespace Blackjack.Data
 {
     public class Card
     {
-        private BitmapImage _cardImg { get; set; }
-        private BitmapImage _cardBack { get; set; }
-
+        private string _cardImg { get; set; }
+        private string _cardBack { get; set; }
+        private string Name { get; set; }
         private CardValue _value { get; set; }
         private SuiteType _suit { get; set; }
         private bool _beenUsed { get; set; }
 
         public Card( CardValue Value, SuiteType Suit )
         {
+            string pathtoCertCard;
+            string pathToCardDir = @"Blackjack.Data\Resources\card_images";
+            string baseCardDir = 
+                Directory.GetParent(
+                    Directory.GetParent(
+                        Directory.GetParent(
+                            Directory.GetParent(
+                                Directory.GetCurrentDirectory()
+                            ).FullName
+                        ).FullName
+                    ).FullName
+                ).FullName;
+            string cardImgDir = Path.Combine(baseCardDir, pathToCardDir );
+
             _value = Value;
             _suit = Suit;
             _beenUsed = false;
 
-            #region Set Card Image
-
-            switch (Value)
+            foreach (var suit in Enum.GetValues( typeof( SuiteType ) ))
             {
-                case CardValue.Ace:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Ace_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Ace_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Ace_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Ace_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Two:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Two_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Two_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Two_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Two_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Three:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Three_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Three_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Three_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Three_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Four:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Four_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Four_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Four_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Four_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Five:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Five_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Five_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Five_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Five_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Six:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Six_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Six_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Six_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Six_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Seven:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Seven_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Seven_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Seven_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Seven_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Eight:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Eight_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Eight_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Eight_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Eight_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Nine:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Nine_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Nine_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Nine_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Nine_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Ten:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Ten_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Ten_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Ten_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Ten_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Jack:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Jack_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Jack_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Jack_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Jack_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.Queen:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.Queen_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.Queen_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.Queen_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.Queen_of_Diamonds);
-                            break;
-                    }
-                    break;
-                case CardValue.King:
-                    switch (Suit)
-                    {
-                        case SuiteType.Hearts:
-                            _cardImg = ConvertToBitmapImage(Resources.King_of_Hearts);
-                            break;
-                        case SuiteType.Clubs:
-                            _cardImg = ConvertToBitmapImage(Resources.King_of_Clubs);
-                            break;
-                        case SuiteType.Spades:
-                            _cardImg = ConvertToBitmapImage(Resources.King_of_Spades);
-                            break;
-                        case SuiteType.Diamonds:
-                            _cardImg = ConvertToBitmapImage(Resources.King_of_Clubs);
-                            break;
-                    }
-                    break;
+                foreach (var value in Enum.GetValues( typeof( CardValue ) ))
+                {
+                    pathtoCertCard = @$"{suit}\{value} of {suit}.png";
+                    this._cardImg = Path.Combine(cardImgDir, pathtoCertCard);
+                    pathtoCertCard = string.Empty;
+                }
             }
 
-            BitmapImage bitmapImage = new BitmapImage();
-            Bitmap CardBack = Resources.Card_Back;
-            using (System.IO.MemoryStream memory = new System.IO.MemoryStream())
-            {
-                CardBack.Save( memory, System.Drawing.Imaging.ImageFormat.Png );
-                memory.Position = 0;
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-            }
-
-            _cardBack = bitmapImage;
-            #endregion
+            _cardBack = Path.Combine( cardImgDir, "Card Back.png");
         }
 
         public CardValue GetCardValue( ) => _value;
@@ -278,15 +62,15 @@ namespace Blackjack.Data
 
         public SuiteType SuiteType1 => _suit;
 
-        public BitmapImage Image
-            => _cardImg;
+        //public BitmapImage Image
+        //    => _cardImg;
 
-        public BitmapImage BackImage 
-            => _cardBack;
+        //public BitmapImage BackImage
+        //    => _cardBack;
 
-        public void SetImage( BitmapImage CardImage ) => _cardImg = CardImage;
+        //public void SetImage( BitmapImage CardImage ) => _cardImg = CardImage;
 
-        internal BitmapImage ConvertToBitmapImage(byte[] ImageBytes)
+        internal BitmapImage ConvertToBitmapImage( byte[] ImageBytes )
             => (BitmapImage)(BitmapSource)new ImageSourceConverter().ConvertFrom( ImageBytes );
     }
 }
